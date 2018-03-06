1478a63b52e8522c820aa68d22d84120e829154c
tangxiaozhuo <tangxiaozhuo001@lianjia.com>
2018-03-06 12:01:16 +0800

    基础搭建

--- react-app/src/App.js
+++ react-app/src/App.js
@@ -1,19 +1,27 @@
-import React, { Component } from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {Component} from "react";
+import "./App.css";
+import {BrowserRouter as Router, Route} from "react-router-dom";
+import {Layout} from "antd";
+import MyHeader from "./components/Header";
+import MySider from './components/Sider'
+
+const {Header, Sider, Content, Footer} = Layout
+
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
-        </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
-      </div>
+      <Router>
+        <Layout style={{backgroundColor:"#fff"}}>
+          <Route path="/" component={MyHeader}></Route>
+          <Layout>
+            <Route path="/:channel" component={MySider}></Route>
+
+            <Content style={{backgroundColor: "#fff", height: "200px"}}></Content>
+          </Layout>
+          <Footer style={{backgroundColor: "#fff", height: "200px"}}></Footer>
+        </Layout>
+      </Router>
     );
   }
 }
